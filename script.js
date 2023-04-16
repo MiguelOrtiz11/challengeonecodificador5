ca25da686d25505dd44397e34f4b8a2d2b844ef9
Miguel Ortiz <elsuperm2016@gmail.com>
2023-04-16 00:40:23 -0500

    Comentando el codigo para mejor entendimiento

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
+//agrega valor del output como variable
 var textareaOutput = document.getElementById("textareaOutput");
 
+//no haya texto visible en el input
 document.getElementById ("textareaInput").value = "";
 
 var mensaje = "";
 
+//obtener valores de los botones
 var btnEncriptar = document.getElementById("btnEncriptar");   
 var btnDesencriptar = document.getElementById("btnDesencriptar");
 var btnCopiar = document.getElementById("btnCopiar");
 
+//toma ingreso de datos, encriptarlo y mostrar resultado en otro cuadro de texto
 function encriptar (){
     var textareaInput = document.getElementById ("textareaInput").value;
+    //muestra el mensaje encriptado
     mensaje = encriptarTexto(textareaInput);
     document.querySelector(".textareaOutput").value = mensaje;
     document.getElementById("outputContainer").style.visibility = "visible";
     document.getElementById("textos").style.display = "none";    
     document.querySelector(".buttonRight").style.display = "flex";
+    //elimina imagen para mostrar cambio
     document.getElementById("sectionRight").style.backgroundImage = "none"; 
 }
 
+
+//mensaje de confirmacion de encriptado y revertir encriptacion
 function desencriptar (){
     var textareaInput = document.getElementById ("textareaInput").value;
+    //muestra el mensaje desencriptado
     mensaje = desencriptarTexto(textareaInput);
     document.querySelector(".textareaOutput").value = mensaje;
 }
 
+//encriptar el texto
 function encriptarTexto(mensaje){
     var textoEncriptado = mensaje
     .replaceAll("e", "enter")
@@ -34,6 +44,7 @@ function encriptarTexto(mensaje){
     return textoEncriptado;
 }
 
+//desencriptar el texto
 function desencriptarTexto(mensaje){
     var textoEncriptado = mensaje
     .replaceAll("enter", "e")
@@ -44,11 +55,13 @@ function desencriptarTexto(mensaje){
     return textoEncriptado;
 }
 
+//funcion de copiar el texto y retornarlo en el input de texto
 function copiar () {
     navigator.clipboard.writeText(document.querySelector(".textareaOutput").value);
     document.querySelector(".textareaInput").value = document.querySelector(".textareaOutput").value;
 }
 
+//da funcion a las variables cuando se de click a los botones especificos
 btnEncriptar.onclick = encriptar;
 btnDesencriptar.onclick = desencriptar;
 btnCopiar.onclick = copiar; 
